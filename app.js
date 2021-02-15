324f7694f4a3fc7fb86c9eba0fe8b84ddb4683c0
derekchancts-at-878661647005 <derekchancts@yahoo.com.hk>
2021-02-16 03:25:15 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/', (req, res) => {
+  res.send("Welcome to the home page")
+})
+
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`server running on port ${port}`));
